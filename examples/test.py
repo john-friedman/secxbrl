dad1d554ab903fe8ef9378c0f6e2da32b0239f53
John Friedman <jgfriedman99@gmail.com>
2025-06-21 19:15:47 -0700

    ready for public

--- examples/test.py
+++ examples/test.py
@@ -1,8 +1,5 @@
 from secxbrl import parse_inline_xbrl
-from time import time
-import json
 
-s = time()
 path = '../samples/000095017022000796/tsla-20211231.htm'
 with open(path,'rb') as f:
     content = f.read()
@@ -12,6 +9,5 @@ with open('test.txt','w', encoding='utf-8') as f:
     f.writelines([str(item)+'\n\n' for item in ix])
 
 # get all EarningsPerShareBasic
-
 basic = [{'val':item['_val'],'date':item['_context']['context_period_enddate']} for item in ix if item['_attributes']['name']=='us-gaap:EarningsPerShareBasic']
 print(basic)
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
-import os
-print(os.getcwd())
 
 with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="secxbrl",
-    version="0.0.2",
+    version="0.0.3",
     description="A package to parse SEC XBRL",
     packages=find_packages(),
     install_requires=['selectolax'],
