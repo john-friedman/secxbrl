cc6c73701532435392a56d9be49946e366eeaa51
John Friedman <jgfriedman99@gmail.com>
2025-06-21 19:09:55 -0700

    added readme to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="secxbrl",
-    version="0.0.1",
+    version="0.0.2",
+    description="A package to parse SEC XBRL",
     packages=find_packages(),
     install_requires=['selectolax'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
