50478941e73c681d252021c6ac33126d7c6d7746
John Friedman <jgfriedman99@gmail.com>
2025-06-21 19:18:56 -0700

    added mit license

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="secxbrl",
-    version="0.0.3",
+    version="0.0.4",
     description="A package to parse SEC XBRL",
     packages=find_packages(),
     install_requires=['selectolax'],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT"
 )
