7acc58326e599fdbed70761fc27390094c5e5836
John Friedman <jgfriedman99@gmail.com>
2025-06-21 19:11:38 -0700

    lol

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
+import os
+print(os.getcwd())
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
