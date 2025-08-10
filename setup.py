79f9adec9a9667f6ada30b43aab391016f403ce5
John Friedman <jgfriedman99@gmail.com>
2025-08-09 23:45:15 -0700

    added auto decode from bytes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="secxbrl",
-    version="0.1.0",
+    version="0.1.1",
     description="A package to parse SEC XBRL",
     packages=find_packages(),
     install_requires=['selectolax'],
